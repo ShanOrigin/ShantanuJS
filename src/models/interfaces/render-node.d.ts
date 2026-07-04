4906e6886a3d2fa180312b6ff73afcf342d56c9e
The-Suryawanshi-Shantanu <gv2422244242@gmail.com>
2026-07-04 15:45:03 +0530

    [ RenderNode ] :
    Expose animation and event APIs
    
    - Add strongly typed animate and animation methods
    - Integrate IAnimationOptions into RenderNode interface
    - Add complete event API (on, off, once)
    - Expose event handler lookup methods for dispatcher
    - Align RenderNode contract with animation and event components

--- src/models/interfaces/render-node.d.ts
+++ src/models/interfaces/render-node.d.ts
@@ -19,6 +19,8 @@ import type {
 } from '../types/graphics-model';
 import type { IGraphicsModel } from './graphics-model';
 import type { ValidGraphicsShapes } from '../types/graphics-model';
+import { IAnimationOptions } from '../types/animation/options';
+import { IAnimation } from './animation';
 
 export interface IRenderNode<T extends ValidGraphicsShapes>
   extends IGraphicsModel<T> {
@@ -49,44 +51,75 @@ export interface IRenderNode<T extends ValidGraphicsShapes>
    * --------------------------------------------------------------------------
    */
 
-  //animate(...args: any[]): any;
+  animate(props: IAnimationOptions<T>): void;
+  animation(
+    props: IAnimationOptions<T>
+  ): Omit<IAnimation, 'animate' | 'update'>;
 
   /*
    * --------------------------------------------------------------------------
    * EVENT CONVENIENCE API
    * --------------------------------------------------------------------------
    */
-  /*
-  on(...args: any[]): any;
 
-  off(...args: any[]): any;
+  /**
+   * Registers an event handler.
+   *
+   * Multiple handlers may be registered for the same event.
+   *
+   * @param event The event to subscribe to.
+   * @param callback The handler to invoke when the event is dispatched.
+   */
+  on(event: SupportedEvents, callback: Handler): void;
 
-  once(...args: any[]): any;
-*/
+  /**
+   * Removes event handlers associated with an event.
+   *
+   * If `callback` is provided, only that handler is removed.
+   * Otherwise, all handlers registered for the event are removed.
+   *
+   * @param event The event to unsubscribe from.
+   * @param callback Optional handler to remove.
+   */
+  off(event: SupportedEvents, callback?: Handler): void;
+
+  /**
+   * Registers an event handler that is invoked at most once.
+   *
+   * After the first invocation, the handler is automatically removed.
+   *
+   * @param event The event to subscribe to.
+   * @param callback The handler to invoke once.
+   */
+  once(event: SupportedEvents, callback: Handler): void;
+
+  /**
+   * Returns handler for given event type.
+   *
+   * IMPORTANT:
+   * - Intended ONLY for EventSystem usage
+   * - Not part of public contract
+   *
+   * @param type Event type
+   * @returns Handler or undefined
+   *
+   */
+
+  getEventHandler(type: SupportedEvents): Handler | void;
+
+  /**
+   * Checks whether a handler exists for given event type.
+   *
+   * Useful for fast path skipping in dispatcher.
+   */
+  hasEventHandler(type: SupportedEvents): boolean;
+
   /*
    * --------------------------------------------------------------------------
    * COMPONENT ACCESSORS
    * --------------------------------------------------------------------------
    */
   /*
-  transform: {
-    transform(...args: any[]): any;
-
-    startBatching(...args: any[]): any;
-
-    endBatching(...args: any[]): any;
-  };
-
-  animation: {
-    play(...args: any[]): any;
-
-    pause(...args: any[]): any;
-
-    resume(...args: any[]): any;
-
-    stop(...args: any[]): any;
-  };
-
   filter: {
     brightness(...args: any[]): any;
 
