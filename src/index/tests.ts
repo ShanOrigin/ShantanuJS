07d3c336e3376da60ee978fc543716712fb5f3ea
The-Suryawanshi-Shantanu <gv2422244242@gmail.com>
2026-07-30 17:58:42 +0530

    [ Testing ] : - Refactored the test runner to support module-based test execution instead of a single global method dispatcher. - Introduced dedicated dispatchers for canvas and shape test suites to improve organization and scalability. - Added support for executing canvas creation tests through the centralized test entry point. - Updated the test launcher to accept both module and method identifiers for more flexible test selection. - Configured the default test entry to execute the canvas creation test during development.

--- src/index/tests.ts
+++ src/index/tests.ts
@@ -5,8 +5,24 @@ import { toBackMethod } from "../tests/tests/toBackMethod.js";
 import { toFrontMethod } from "../tests/tests/toFrontMerhod.js";
 
 import { cloneMethod } from "../tests/tests/cloneMethod.js";
+
+import { createCanvas } from "../tests/tests/canvas/create.js";
 // Entry function (user-defined)
-export function runTests(method: string) {
+export function runTests(module: string, method: string) {
+  switch (module) {
+    case "canvas":
+      canvasMethods(method);
+      break;
+    case "shape":
+      shapeBasicMethod(method);
+      break;
+
+    default:
+      break;
+  }
+}
+
+function shapeBasicMethod(method: string) {
   switch (method) {
     case "hide":
       hideMethod();
@@ -29,5 +45,17 @@ export function runTests(method: string) {
   }
 }
 
-const method = "clone";
-setTimeout(() => runTests(method), 5000);
+function canvasMethods(method: string) {
+  switch (method) {
+    case "create":
+      createCanvas();
+      break;
+
+    default:
+      break;
+  }
+}
+
+const module = "canvas";
+const method = "create";
+setTimeout(() => runTests(module, method), 5000);
